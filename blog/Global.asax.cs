0c93b9f6918f1c411546e13c1203486871b53efb
Robert Zuba <robert.szymon.zuba@gmail.com>
2016-07-17 00:18:08 +0100

    Działajacy blog

--- blog/Global.asax.cs
+++ blog/Global.asax.cs
@@ -16,6 +16,7 @@ namespace blog
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
--- blog/entity/Post.cs
+++ blog/entity/Post.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace blog.entity
 {
     public class Post
     {
+        public Post()
+        {
+
+        }
+
+
+        public int PostId { get; set; }
+
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Body { get; set; }
 
         public string Author { get; set; }
-
-        public string DateCreated { get; set; }
+        public DateTime DateCreated { get; set; }
 
         public virtual ICollection<Comment> Comments { get; set; }
     }
