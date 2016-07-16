1bb8a0f4c0bb8495d23d19e11aad903e80298bbf
Robert Zuba <robert.szymon.zuba@gmail.com>
2016-07-16 19:14:12 +0100

    Post i Comment dodane

---
+++ blog/entity/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace blog.entity
+{
+    public class Comment
+    {
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public DateTime AddedTime { get; set; }
+
+        public virtual Post AssignedPost { get; set; }
+
+
+    }
+}
---
+++ blog/entity/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace blog.entity
+{
+    public class Post
+    {
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public string Author { get; set; }
+
+        public string DateCreated { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
